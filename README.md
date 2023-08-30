7d50e2d7c410b3a492e1db816089d14c5105436f
Johann <40876495+joFolta@users.noreply.github.com>
2023-08-30 18:38:32 -0400

    Reset repo with working boilerplate: "material-ui-nextjs-ts" example from material-ui's github

--- README.md
+++ README.md
@@ -1,52 +1,34 @@
-# washington-weather-hq
+# Material UI - Next.js App Router example in TypeScript
 
+This is a [Next.js](https://nextjs.org/) project bootstrapped using [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app) with Material UI installed.
 
-Mt Washington weather web app built with NextJS, Typescript
-**DISCLAIMER: This demo app is for educational purposes only. Not intended for use in a production app.** 
+## How to use
 
-### Technologies used:
-- https://github.com/niutech/x-frame-bypass
-- https://mui.com/, https://github.com/mui/material-ui/tree/v5.14.7/docs/data/material/getting-started/templates/dashboard
+Download the example [or clone the repo](https://github.com/mui/material-ui):
 
-### Hurdles
-#### Integration issues of NextJS with MaterialUI 
-- e.g. Error: Attempted to call styled() from the server but styled is on the client. It's not possible to invoke a clienton from the server, it can only be rendered as a Component or passed to props of a Client Component.
-- https://mui.com/material-ui/guides/next-js-app-router/ 
-- https://github.com/mui/material-ui/tree/master/examples/material-ui-nextjs-ts 
-
-------------------
-
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
-
-## Getting Started
-
-First, run the development server:
+<!-- #default-branch-switch -->
 
 ```bash
+curl https://codeload.github.com/mui/material-ui/tar.gz/master | tar -xz --strip=2  material-ui-master/examples/material-ui-nextjs-ts
+cd material-ui-nextjs-ts
+```
+
+Install dependencies and run:
+
+```bash
+npm install
 npm run dev
-# or
-yarn dev
-# or
-pnpm dev
 ```
 
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
 
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
+You can start editing the page by modifying `src/app/page.tsx`. The page auto-updates as you edit the file.
 
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
+This example uses [`next/font/google`](https://nextjs.org/docs/app/building-your-application/optimizing/fonts#google-fonts) to automatically optimize and load Roboto, a custom Google Font.
 
-## Learn More
+## Learn more
 
-To learn more about Next.js, take a look at the following resources:
+To learn more about this example:
 
 - [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+- [Customizing Material UI](https://mui.com/material-ui/customization/how-to-customize/) - approaches to customizing Material UI.
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,59 +1,72 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-
+import * as React from 'react';
 import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import Container from '@mui/material/Container';
+import Grid from '@mui/material/Unstable_Grid2';
+import Drawer from '@mui/material/Drawer';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import EngineeringIcon from '@mui/icons-material/Engineering';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import Alert from '@mui/material/Alert';
+import AlertTitle from '@mui/material/AlertTitle';
+import MediaCard from '@/components/MediaCard';
 
-import Dashboard from './components/Dashboard'
-
-
-{/* <script src="https://unpkg.com/@ungap/custom-elements-builtin"></script> */ }
-{/* <script type="module" src="https://unpkg.com/x-frame-bypass"></script>
-<iframe is="x-frame-bypass" src="https://news.ycombinator.com/"></iframe> */}
-
-export default function Home() {
+export default function HomePage() {
   return (
-    <main>
-      <Container>
-        <Box>
-          <Card>
-            <EngineeringIcon fontSize="large" />
-            <Button variant="contained" color="success">
-              Success
-            </Button>
-          </Card>
-        </Box>
-      </Container>
-      <h1>Washington Weather HQ</h1>
-      <Dashboard />
-      {/* <iframe src="https://www.mountain-forecast.com/peaks/Mount-Washington-2/forecasts/1917" width="100%"
-        height="4500" loading="lazy">
-      </iframe> */}
-      {/* Alltrails */}
-      {/* <iframe src="https://www.alltrails.com/us/new-hampshire/mount-washington" width="100%"
-        height="400px" loading="lazy">
-      </iframe> */}
-      {/* weather.com */}
-      {/* <iframe src="https://weather.com/weather/tenday/l/c0292db17aea257fa09360f7942149ec5d9cd9e9e319f8bcff6bf11b111d6253" width="100%"
-        height="400px" loading="lazy">
-      </iframe> */}
-      {/* Weather Station */}
-      {/* <div id={styles.weatherStationContainer}>
-        <iframe id={styles.weatherStation} src="https://www.mountwashington.org/experience-the-weather/mount-washington-weather.aspx#innerContent" width="80%"
-          height="500px" loading="lazy">
-        </iframe>
-      </div> */}
-
-      {/* TODO USE div #innerContent */}
-      {/* Youtube live videos */}
-      {/* TODO: use carousel */}
-      {/* <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/9cOe77NwBMk?si=-YmY2hQfQKpqEQy3" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
-      <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/RUN2G9r136c?si=jd9VkaFJlNyAQfiQ" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
-      <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/5qVHjf7hKZU?si=4sFvjthHC_FVDpVL" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe> */}
-    </main >
-  )
+    <Box sx={{ display: 'flex' }}>
+      <div>
+        <Alert severity="info" sx={{ mt: 2, mb: 5 }}>
+          <AlertTitle>Hello 👋</AlertTitle>
+          This app uses the Next.js App Router and Material UI v5.
+        </Alert>
+        <Grid container rowSpacing={3} columnSpacing={3}>
+          <Grid xs={6}>
+            <MediaCard
+              heading="CMYK"
+              text="The CMYK color model (also known as process color, or four color) is a subtractive color model, based on the CMY color model, used in color printing, and is also used to describe the printing process itself."
+            />
+          </Grid>
+          <Grid xs={6}>
+            <MediaCard
+              heading="HSL and HSV"
+              text="HSL (for hue, saturation, lightness) and HSV (for hue, saturation, value; also known as HSB, for hue, saturation, brightness) are alternative representations of the RGB color model, designed in the 1970s by computer graphics researchers."
+            />
+          </Grid>
+          <Grid xs={6}>
+            <MediaCard
+              heading="RGB"
+              text="An RGB color space is any additive color space based on the RGB color model. RGB color spaces are commonly found describing the input signal to display devices such as television screens and computer monitors."
+            />
+          </Grid>
+          <Grid xs={6}>
+            <MediaCard
+              heading="CIELAB"
+              text="The CIELAB color space, also referred to as L*a*b*, was intended as a perceptually uniform space, where a given numerical change corresponds to a similar perceived change in color."
+            />
+          </Grid>
+        </Grid>
+      </div>
+      <Drawer
+        sx={{
+          width: 320,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: 320,
+            boxSizing: 'border-box',
+            top: ['48px', '56px', '64px'],
+            height: 'auto',
+            bottom: 0,
+          },
+        }}
+        variant="permanent"
+        anchor="right"
+      >
+        <List sx={{ px: 2 }}>
+          <ListItem disablePadding>
+            <Typography variant="overline" sx={{ fontWeight: 500 }}>
+              On this page
+            </Typography>
+          </ListItem>
+        </List>
+      </Drawer>
+    </Box>
+  );
 }
